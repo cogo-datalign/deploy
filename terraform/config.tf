91f113726ef5d0eccedd070cafbd9edddaa2b42a
Autumn Patterson <apatterson@cogolabs.com>
2024-04-22 17:54:12 -0600

    We got a working config!!!!

--- terraform/config.tf
+++ terraform/config.tf
@@ -4,19 +4,24 @@ provider "aws" {
 
 module "eks" {
   source         = "terraform-aws-modules/eks/aws"
-  cluster_name   = "Test-Blue-Cluster"
+  cluster_name   = "Test-Green-Cluster-fifth"
   cluster_version = "1.28"
 
   vpc_id = "vpc-0fe2c01a1360765e3"
   subnet_ids = ["subnet-07016ef218378d92f", "subnet-0f6c2f95e7ba90911", "subnet-074cf248e712ccbb5", "subnet-03eeb45e200a89454"]  # Specify your VPC subnets
 
   eks_managed_node_groups = {
-    one = {
-      desired_capacity = 2
-      max_capacity     = 4
-      min_capacity     = 1
+    fifth = {
+      desired_size = 4
+      max_size     = 6
+      min_size     = 3
 
-      instance_type = "t3.medium"
+      labels = {
+        role = "blue-green-node-set"
+      }
+
+      instance_types = ["t2.medium"]
+      capacity_type = "ON_DEMAND"
     }
   }
 }
