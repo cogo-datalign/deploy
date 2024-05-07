2cc8b448d971f5b9d1d2ff4c7a1b0754187f6951
Nick Chiappari <nick@datalign.com>
2024-05-06 20:07:37 -0400

    in prog

--- terraform/config.tf
+++ terraform/config.tf
@@ -4,14 +4,14 @@ provider "aws" {
 
 module "eks" {
   source         = "terraform-aws-modules/eks/aws"
-  cluster_name   = "Test-Green-Cluster-fifth"
+  cluster_name   = "Test-Green-Cluster-six"
   cluster_version = "1.28"
 
   vpc_id = "vpc-0fe2c01a1360765e3"
   subnet_ids = ["subnet-07016ef218378d92f", "subnet-0f6c2f95e7ba90911", "subnet-074cf248e712ccbb5", "subnet-03eeb45e200a89454"]  # Specify your VPC subnets
 
   eks_managed_node_groups = {
-    fifth = {
+    six = {
       desired_size = 4
       max_size     = 6
       min_size     = 3
@@ -25,3 +25,5 @@ module "eks" {
     }
   }
 }
+
+
