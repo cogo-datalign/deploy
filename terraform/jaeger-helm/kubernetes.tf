ab5ee9359434f4693a5d13d8770d6041a9f44ab2
Nick Chiappari <nick@datalign.com>
2024-07-15 18:36:20 -0400

    minor updates

--- terraform/jaeger-helm/kubernetes.tf
+++ terraform/jaeger-helm/kubernetes.tf
@@ -29,8 +29,6 @@ provider "kubernetes" {
     args        = ["eks", "get-token", "--cluster-name", data.aws_eks_cluster.cluster.name]
     command     = "aws"
   }
-  config_path = "~/.kube/config"
-  token = "k8s-aws-v1.aHR0cHM6Ly9zdHMudXMtZWFzdC0xLmFtYXpvbmF3cy5jb20vP0FjdGlvbj1HZXRDYWxsZXJJZGVudGl0eSZWZXJzaW9uPTIwMTEtMDYtMTUmWC1BbXotQWxnb3JpdGhtPUFXUzQtSE1BQy1TSEEyNTYmWC1BbXotQ3JlZGVudGlhbD1BS0lBNFlRRlQzQVRDMzdVQkhOVSUyRjIwMjQwNTE3JTJGdXMtZWFzdC0xJTJGc3RzJTJGYXdzNF9yZXF1ZXN0JlgtQW16LURhdGU9MjAyNDA1MTdUMjEwNDIwWiZYLUFtei1FeHBpcmVzPTYwJlgtQW16LVNpZ25lZEhlYWRlcnM9aG9zdCUzQngtazhzLWF3cy1pZCZYLUFtei1TaWduYXR1cmU9OTRjNDFjNTk1YjE5MWQ5NjY5Y2U5ODQyOWE5NzFkMWU3OGU1ODIyNzNiMjBhOTFiNjBjYWY3OTc5MzExNGY3OA"
 }
 
 data "kubernetes_service" "nginx" {
