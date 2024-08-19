367df6e073d7abcfd35d8e6baa4120a9e2408bfb
Josh Elbahrawy <josh@datalign.com>
2024-08-19 19:51:38 -0400

    jelbahrawy-2979
    figured out how to mess with cluster security groups

--- terraform/config.tf
+++ terraform/config.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 module "eks" {
   source         = "terraform-aws-modules/eks/aws"
-  cluster_name   = "Test-Green-Cluster-ten"
+  cluster_name   = "Test-Green-Cluster-eleven"
   cluster_version = "1.28"
   cluster_endpoint_public_access = true
   cluster_endpoint_private_access = true
@@ -13,7 +13,7 @@ module "eks" {
   subnet_ids = ["subnet-07016ef218378d92f", "subnet-0f6c2f95e7ba90911", "subnet-074cf248e712ccbb5", "subnet-03eeb45e200a89454"]  # Specify your VPC subnets
 
   eks_managed_node_groups = {
-    six = {
+    eleven = {
       desired_size = 4
       max_size     = 6
       min_size     = 3
@@ -26,10 +26,21 @@ module "eks" {
       capacity_type = "ON_DEMAND"
     }
   }
+
+  cluster_security_group_additional_rules = {
+    ingress_ec2_tcp = {
+      description                = "Access EKS from EC2 instance."
+      protocol                   = "tcp"
+      from_port                  = 443
+      to_port                    = 443
+      type                       = "ingress"
+      cidr_blocks                = ["0.0.0.0/0"]
+    }
+  }
 }
 
-resource "aws_eks_access_entry" "ten" {
-  cluster_name = "Test-Green-Cluster-ten"
+resource "aws_eks_access_entry" "eleven" {
+  cluster_name = "Test-Green-Cluster-eleven"
   principal_arn = "arn:aws:iam::877258856486:user/leadalign-eks-master"
   type="STANDARD"
 }
